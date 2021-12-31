b0e0a8dd65f222fe89e766a1b045491ce9b04300
Matteo Carrara <matte.carra@gmail.com>
2021-12-31 01:53:31 +0100

    initial work toe build libcurl for m1

--- build_libcurl_dist.sh
+++ build_libcurl_dist.sh
@@ -16,13 +16,22 @@ echo "#include \"include/curl/curlver.h\"
 function build_for_arch() {
   ARCH=$1
   HOST=$2
-  SYSROOT=$3
+  SDK=$3
   PREFIX=$4
-  IPHONEOS_DEPLOYMENT_TARGET="6.0"
+  DEPLOYMENT_TARGET="6.0"
   export PATH="${DEVROOT}/usr/bin/:${PATH}"
-  export CFLAGS="-DCURL_BUILD_IOS -arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET} -fembed-bitcode"
-  export LDFLAGS="-arch ${ARCH} -isysroot ${SYSROOT}"
-  ./configure --disable-shared --without-zlib --enable-static --enable-ipv6 ${SSL_FLAG} --host="${HOST}" --prefix=${PREFIX} && make -j8 && make install
+  export CFLAGS="-arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot $(xcrun -sdk $SDK --show-sdk-path) -m$SDK-version-min=$DEPLOYMENT_TARGET -fembed-bitcode"
+  ./configure --disable-shared --enable-static --enable-ipv6 --host="${HOST}" --prefix=${PREFIX} --with-secure-transport && make -j8 && make install
+}
+
+function build_for_current_arch() {
+  ARCH=$1
+  HOST=$2
+  SDK=$3
+  PREFIX=$4
+  DEPLOYMENT_TARGET="14.0"
+  export CFLAGS="-arch $ARCH -isysroot $(xcrun -sdk $SDK --show-sdk-path) -m$SDK-version-min=$DEPLOYMENT_TARGET"
+  ./configure --disable-shared --enable-static --enable-ipv6 --host="${HOST}" --prefix=${PREFIX} --with-secure-transport && make -j8 && make install
 }
 
 if [ "${1:-''}" == "openssl" ]
@@ -35,25 +44,25 @@ then
   export SSL_FLAG=--with-ssl=${HOME}/Desktop/openssl-ios-dist
 else
   check_curl_ver
-  export SSL_FLAG=--with-darwinssl
+  export SSL_FLAG="--with-secure-transport"
 fi
 
 TMP_DIR=/tmp/build_libcurl_$$
 
-build_for_arch i386 i386-apple-darwin ${XCODE_DEV}/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/i386 || exit 1
-build_for_arch x86_64 x86_64-apple-darwin ${XCODE_DEV}/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/x86_64 || exit 2
-build_for_arch arm64 arm-apple-darwin ${XCODE_DEV}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/arm64 || exit 3
-build_for_arch armv7s armv7s-apple-darwin ${XCODE_DEV}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7s || exit 4
-build_for_arch armv7 armv7-apple-darwin ${XCODE_DEV}/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7 || exit 5
+build_for_arch armv7s armv7s-apple-darwin iphoneos ${TMP_DIR}/armv7s || exit 4
+build_for_arch armv7 armv7-apple-darwin iphoneos ${TMP_DIR}/armv7 || exit 5
+build_for_current_arch arm64 arm-apple-darwin iphoneos ${TMP_DIR}/arm64 || exit 3
+build_for_current_arch arm64 arm-apple-darwin iphonesimulator ${TMP_DIR}/arm64_sim  || exit 6
 
 mkdir -p ${TMP_DIR}/lib/
 ${DEVROOT}/usr/bin/lipo \
-  -arch x86_64 ${TMP_DIR}/x86_64/lib/libcurl.a \
   -arch armv7 ${TMP_DIR}/armv7/lib/libcurl.a \
   -arch armv7s ${TMP_DIR}/armv7s/lib/libcurl.a \
   -arch arm64 ${TMP_DIR}/arm64/lib/libcurl.a \
   -output ${TMP_DIR}/lib/libcurl.a -create
 
+
+cp ${TMP_DIR}/arm64_sim/lib/libcurl.a ${TMP_DIR}/lib/libcurlsim.a
 cp -r ${TMP_DIR}/arm64/include ${TMP_DIR}/
 
 mkdir -p ${DIST_DIR}
